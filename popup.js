9604c41a4b7a4bd6cf42448f4649b3779d33bbbe
yazshrilld <yazshrilld@gmail.com>
2024-07-16 07:30:42 +0100

    added a button

--- popup.js
+++ popup.js
@@ -1,8 +1,13 @@
 const uName = document.getElementById("uName");
 const messageBody = document.querySelector(".welcome");
+const butn = document.querySelector(".btn")
 
 uName.addEventListener("keydown", (event) => {
   if (event.key === "Enter") {
     messageBody.textContent = `Welcome ${uName.value}`;
   }
 });
+
+butn.addEventListener("click", () => {
+  console.log("Click event listener was added")
+})
