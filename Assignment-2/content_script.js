45c6f2e8e1372f17f16564747719946c79b4e6c4
yazshrilld <yazshrilld@gmail.com>
2024-07-19 08:02:28 +0100

    updated the asignment-2

--- Assignment-2/content_script.js
+++ Assignment-2/content_script.js
@@ -1,31 +1,3 @@
-// Create the button element
-// const button = document.createElement("button");
-// button.textContent = "Log-Me";
-// button.id = "add_me";
-
-// // Create the div element
-// const messageDiv = document.createElement("div");
-// messageDiv.textContent = "I am here";
-// messageDiv.id = "message_div";
-// messageDiv.className = "hidden";
-
-// // Append the button to the body
-// document.body.appendChild(button);
-
-// // Add click event listener to the button
-// button.addEventListener("click", () => {
-//   // Append the div to the body
-//   document.body.appendChild(messageDiv);
-
-//   // Remove the hidden class to display the div
-//   messageDiv.classList.remove("hidden");
-
-//   // Change the button color to red after 5 seconds
-//   setTimeout(() => {
-//     button.style.backgroundColor = "red";
-//   }, 5000);
-// });
-
 const button = document.createElement("button");
 button.textContent = "Log-Me";
 button.id = "add_me";
